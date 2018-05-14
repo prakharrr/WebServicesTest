e26b4a1d459a3bfbfe28ef40d5052ed6b06fe4d4
prakharrr <pxr7185@g.rit.edu>
2018-05-14 16:41:33 -0400

    added countries rest api URL

--- Java Web Services/WebServices/src/Book.java
+++ Java Web Services/WebServices/src/Book.java
@@ -1,6 +1,23 @@
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 public class Book {
 
     public static void main(String[] args) {
+
+        // 1. Create a client
+        Client client = ClientBuilder.newClient();
+
+
+        // 2. Set a target to client
+        WebTarget webTarget = client.target("https://restcountries.eu/rest/v2/all");
+
+        System.out.println(
+                webTarget.request(MediaType.TEXT_XML).get(String.class)
+        );
+
 
     }
 }
